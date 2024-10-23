c7927694f104966616e0f9063c7131294e916d46
slacker-devlpr <shubstay@gmail.com>
2024-10-23 13:14:06 +0200

    Create katja.py

---
+++ katja.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.markdown(
+    f"""
+    <style>
+    h1 {{
+        color: white;  /* Change the text color */
+        text-align: center;  /* Center the text */
+        font-family: 'Arial', sans-serif;  /* Change the font family */
+        font-size: 3em;  /* Change the font size */
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown("<h1>Katja Zakrajsek</h1>", unsafe_allow_html=True)
